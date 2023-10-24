7ea771b3f084c8e0712b72dd0e482540b4abc2e1
grexecution <greg.wallner@gmail.com>
2023-10-24 17:48:30 +0200

    Init commit

---
+++ src/navigation/vertical/index.js
@@ -0,0 +1,205 @@
+const navigation = () => {
+  return [
+
+    {
+      sectionTitle: 'Views'
+    },
+    {
+      icon: 'tabler:smart-home',
+      title: 'Analytics',
+      path: '/dashboards/analytics'
+    },
+    {
+      icon: 'tabler:smart-home',
+      title: 'CRM',
+      path: '/dashboards/crm'
+    },
+    {
+      icon: 'tabler:smart-home',
+      title: 'eCommerce',
+      path: '/dashboards/ecommerce'
+    },
+    {
+      sectionTitle: 'Apps & Pages'
+    },
+    {
+      title: 'Email',
+      icon: 'tabler:mail',
+      path: '/apps/email'
+    },
+    {
+      title: 'Chat',
+      icon: 'tabler:messages',
+      path: '/apps/chat'
+    },
+    {
+      title: 'Calendar',
+      icon: 'tabler:calendar',
+      path: '/apps/calendar'
+    },
+    {
+      title: 'Invoice',
+      icon: 'tabler:file-dollar',
+      children: [
+        {
+          title: 'List',
+          path: '/apps/invoice/list'
+        },
+        {
+          title: 'Preview',
+          path: '/apps/invoice/preview'
+        },
+        {
+          title: 'Edit',
+          path: '/apps/invoice/edit'
+        },
+        {
+          title: 'Add',
+          path: '/apps/invoice/add'
+        }
+      ]
+    },
+    {
+      title: 'User',
+      icon: 'tabler:user',
+      path: '/apps/user/list'
+    },
+    {
+      title: 'Roles & Permissions',
+      icon: 'tabler:settings',
+      children: [
+        {
+          title: 'Roles',
+          path: '/apps/roles'
+        },
+        {
+          title: 'Permissions',
+          path: '/apps/permissions'
+        }
+      ]
+    },
+    {
+      title: 'Wizard Examples',
+      icon: 'tabler:forms',
+      children: [
+        {
+          title: 'Checkout',
+          path: '/pages/wizard-examples/checkout'
+        },
+        {
+          title: 'Property Listing',
+          path: '/pages/wizard-examples/property-listing'
+        },
+        {
+          title: 'Create Deal',
+          path: '/pages/wizard-examples/create-deal'
+        }
+      ]
+    },
+    {
+      sectionTitle: 'Forms & Tables'
+    },
+    {
+      title: 'Form Elements',
+      icon: 'tabler:toggle-left',
+      children: [
+        {
+          title: 'Text Field',
+          path: '/forms/form-elements/text-field'
+        },
+        {
+          title: 'Select',
+          path: '/forms/form-elements/select'
+        },
+        {
+          title: 'Checkbox',
+          path: '/forms/form-elements/checkbox'
+        },
+        {
+          title: 'Radio',
+          path: '/forms/form-elements/radio'
+        },
+        {
+          title: 'Custom Inputs',
+          path: '/forms/form-elements/custom-inputs'
+        },
+        {
+          title: 'Textarea',
+          path: '/forms/form-elements/textarea'
+        },
+        {
+          title: 'Autocomplete',
+          path: '/forms/form-elements/autocomplete'
+        },
+        {
+          title: 'Date Pickers',
+          path: '/forms/form-elements/pickers'
+        },
+        {
+          title: 'Switch',
+          path: '/forms/form-elements/switch'
+        },
+        {
+          title: 'File Uploader',
+          path: '/forms/form-elements/file-uploader'
+        },
+        {
+          title: 'Editor',
+          path: '/forms/form-elements/editor'
+        },
+        {
+          title: 'Slider',
+          path: '/forms/form-elements/slider'
+        },
+        {
+          title: 'Input Mask',
+          path: '/forms/form-elements/input-mask'
+        },
+      ]
+    },
+    {
+      icon: 'tabler:layout-navbar',
+      title: 'Form Layouts',
+      path: '/forms/form-layouts'
+    },
+    {
+      title: 'Form Validation',
+      path: '/forms/form-validation',
+      icon: 'tabler:checkbox'
+    },
+    {
+      title: 'Form Wizard',
+      path: '/forms/form-wizard',
+      icon: 'tabler:text-wrap-disabled'
+    },
+    {
+      title: 'Table',
+      icon: 'tabler:table',
+      path: '/tables/mui'
+    },
+    {
+      title: 'Mui DataGrid',
+      icon: 'tabler:layout-grid',
+      path: '/tables/data-grid'
+    },
+    {
+      sectionTitle: 'Charts & Misc'
+    },
+    {
+      title: 'Charts',
+      icon: 'tabler:chart-pie',
+      children: [
+        {
+          title: 'Apex',
+          path: '/charts/apex-charts'
+        },
+        {
+          title: 'Recharts',
+          path: '/charts/recharts'
+        }
+      ]
+    },
+  ]
+}
+
+export default navigation
